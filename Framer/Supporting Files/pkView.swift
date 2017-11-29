ac421e6bd813bbebb462882299937994c6c6aaa4
Patrick Kladek <patrick.kladek@outlook.com>
2017-11-29 12:02:41 +0100

    add background color property to a view subclass for IB

---
+++ Framer/Supporting Files/pkView.swift
@@ -0,0 +1,44 @@
+//
+//  pkView.swift
+//  Framer
+//
+//  Created by Patrick Kladek on 28.11.17.
+//  Copyright © 2017 Patrick Kladek. All rights reserved.
+//
+
+import Cocoa
+
+
+@IBDesignable
+final class pkView: NSView {
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        self.wantsLayer = true
+    }
+
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+
+        self.wantsLayer = true
+    }
+
+
+    override var wantsUpdateLayer: Bool {
+        return true
+    }
+
+    override func updateLayer() {
+        guard let color = self.backgroundColor else { return }
+
+        self.layer?.backgroundColor = color.cgColor
+    }
+
+
+    @IBInspectable var backgroundColor: NSColor? {
+        didSet {
+            self.needsDisplay = true
+        }
+    }
+}
