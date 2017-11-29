9d48d68c803174a62a596d9bac54be14c727d191
Patrick Kladek <patrick.kladek@outlook.com>
2017-11-29 12:03:58 +0100

    use documentWindowController and add basic saving

--- Framer/Document.swift
+++ Framer/Document.swift
@@ -10,34 +10,32 @@ import Cocoa
 
 class Document: NSDocument {
 
+    private(set) var layers: [LayoutableObject]
+
     override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
+        self.layers = []
+        let root = LayoutableObject(title: "Background", frame: CGRect(x: 0, y: 0, width: 800, height: 1200), file: "../background/01", root: true)
+        self.layers.append(root)
     }
 
     override class var autosavesInPlace: Bool {
         return true
     }
 
-    override var windowNibName: NSNib.Name? {
-        // Returns the nib file name of the document
-        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
-        return NSNib.Name("Document")
+    override func makeWindowControllers() {
+        let windowController = DocumentWindowController()
+        self.addWindowController(windowController)
     }
 
     override func data(ofType typeName: String) throws -> Data {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+        return try encoder.encode(self.layers)
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let decoder = JSONDecoder()
+        self.layers = try decoder.decode([LayoutableObject].self, from: data)
     }
-
-
 }
 
